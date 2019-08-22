ac5aa756d8f85c5a95fb5d5fb512777b039c0497
Mateus Felix <mateus.felix@NOT-M000634.future.corp>
2019-08-22 17:45:42 -0300

    chore(cli): initial config

---
+++ src/cli.js
@@ -0,0 +1,70 @@
+import arg from 'arg'
+import inquirer from 'inquirer'
+import createProject from './main'
+
+const parseArgumentsIntoOptions = rawArgs => {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install',
+    },
+    {
+      argv: rawArgs.slice(2),
+    },
+  )
+
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    projectName: args._[0],
+    runInstall: args['--install'] || false,
+  }
+}
+
+const promptForMissingOptions = async options => {
+  if (options.skipPrompts) {
+    return {
+      ...options,
+    }
+  }
+
+  const questions = []
+
+  if (!options.projectName) {
+    questions.push({
+      type: 'input',
+      name: 'projectName',
+      message: 'You should specify a name for your project:',
+      default: 'iron-api',
+    })
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository?',
+      default: false,
+    })
+  }
+
+  const answers = await inquirer.prompt(questions)
+
+  return {
+    ...options,
+    projectName: options.projectName || answers.projectName,
+    git: options.git || answers.git,
+  }
+}
+
+export const cli = async args => {
+  let options = parseArgumentsIntoOptions(args)
+  options = await promptForMissingOptions(options)
+  await createProject(options)
+}
+
+export default cli
---
+++ src/main.js
@@ -0,0 +1,84 @@
+import chalk from 'chalk'
+import fs from 'fs'
+import ncp from 'ncp'
+import path from 'path'
+import { promisify } from 'util'
+import execa from 'execa'
+import Listr from 'listr'
+import { projectInstall } from 'pkg-install'
+
+const access = promisify(fs.access)
+const copy = promisify(ncp)
+
+const initGit = async options => {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  })
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize Git'))
+  }
+  return false
+}
+
+const copyTemplateFiles = async options =>
+  copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  })
+
+const createProject = async options => {
+  const projectOptions = {
+    ...options,
+    targetDirectory: `${options.targetDirectory || process.cwd()}/${options.projectName}`,
+  }
+
+  const currentFileUrl = import.meta.url
+  const templateDir = path.resolve(new URL(currentFileUrl).pathname, '../../template/iron-api')
+  projectOptions.templateDirectory = templateDir
+
+  try {
+    await access(templateDir, fs.constants.R_OK)
+    try {
+      await access(projectOptions.targetDirectory, fs.constants.R_OK)
+      process.stdout.write('Folder already exists! You must choose another name\n')
+      process.exit(1)
+    } catch (err) {
+      process.stdout.write(
+        `${chalk.blueBright.bold('Project folder:')}${projectOptions.targetDirectory}\n`,
+      )
+    }
+  } catch (err) {
+    process.stdout.write(`${chalk.red.bold('ERROR')} Invalid template name\n`)
+    process.exit(1)
+  }
+
+  const tasks = new Listr([
+    {
+      title: 'Copy project files',
+      task: () => copyTemplateFiles(projectOptions),
+    },
+    {
+      title: 'Initialize git',
+      task: () => initGit(projectOptions),
+      enabled: () => projectOptions.git,
+    },
+    {
+      title: 'Install dependencies',
+      task: () =>
+        projectInstall({
+          cwd: projectOptions.targetDirectory,
+        }),
+      skip: () =>
+        !projectOptions.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined,
+    },
+  ])
+
+  await tasks.run()
+
+  process.stdout.write(`${chalk.green.bold('DONE')} Project ready\n`)
+
+  return true
+}
+
+export default createProject
